cad7696302efc5b51d0e88153aa1c4020c1301ba
saruman9 <rum.274.4@gmail.com>
2023-06-16 13:01:53 +0400

    feat(harness): initial commit

--- src/main.c
+++ src/main.c
@@ -4,15 +4,21 @@
 
 static JniCtx ctx;
 
-int main(int argc, char const *argv[])
-{
+int fuzz(uint8_t *buffer, size_t size) {
     int status;
+    char *options[2] = {"-verbose:jni", "-Djava.library.path=/data/local/tmp"};
 
-    if ((status = init_jni_env(&ctx, NULL, 0)) != JNI_OK)
-    {
+    if ((status = init_jni_env(&ctx, options, 2)) != JNI_OK) {
         return status;
     }
+
     destroy_jni_env(&ctx);
+
+    return 0;
+}
+
+int main(int argc, char const *argv[]) {
+    fuzz(NULL, 0);
 
     return 0;
 }
